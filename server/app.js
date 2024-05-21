606272bad7e8906fedc5e0b043b3e61f6372904e
camilaalbuq <cam.albuquer@gmail.com>
2024-05-21 13:08:23 +0200

    done until day 3

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+// const cors = require("cors");
+const studentRouter = require("./routes/student.routes");
+const cohortRouter = require("./routes/cohort.routes");
+// const authRouter = require("./routes/auth.routes");
+// const userRouter = require("./routes/user.routes");
+
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
+
+//database with mongoose
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -13,19 +26,18 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
-const cors = require("cors");
-const FRONTEND_URL =
-  process.env.ORIGIN || "http://localhost:5005" || "http://localhost:5173";
+// const FRONTEND_URL =
+//   process.env.ORIGIN || "http://localhost:5005" || "http://127.0.0.1:5173";
 
 // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
 // Services like heroku use something called a proxy and you need to add this to your server
 app.set("trust proxy", 1);
 // controls a very specific header to pass headers from the frontend
-app.use(
-  cors({
-    origin: [FRONTEND_URL],
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://127.0.0.1:5173"],
+//   })
+// );
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -48,13 +60,10 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
-app.get("/api/cohorts", (req, res) => {
-  res.sendFile(__dirname + "/cohorts.json");
-});
-
-app.get("/api/students", (req, res) => {
-  res.sendFile(__dirname + "/students.json");
-});
+app.use("/", studentRouter);
+app.use("/", cohortRouter);
+// app.use("/", authRouter);
+// app.use("/", userRouter);
 
 // START SERVER
 app.listen(PORT, () => {
--- server/routes/cohort.routes.js
+++ server/routes/cohort.routes.js
@@ -1,14 +1,111 @@
 const express = require("express");
+const Cohort = require("../models/cohort.model");
 const cohortRouter = express.Router();
 
+cohortRouter.get("/api/cohorts", (req, res, next) => {
+  const { campus, program } = req.query;
+  console.log(program);
+  if (campus && !program) {
+    Cohort.find({ campus: campus })
+      .then((response) => res.status(200).json(response))
+      .catch((error) => res.json(error));
+    return;
+  }
 
-//iteration day 01
-cohortRouter.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "";
+  if (program && !campus) {
+    Cohort.find({ program: program })
+      .then((response) => res.status(200).json(response))
+      .catch((error) => res.json(error));
+    return;
+  }
+  if (campus && program) {
+    Cohort.find({ program: program, campus: campus })
+      .then((response) => res.status(200).json(response))
+      .catch((error) => res.status(500).json(error));
+    return;
+  }
+  Cohort.find()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+      next(error);
+    });
+});
+// cohortRouter.get("/api/cohorts", (req, res) => {
+//   throw new Error("Test error");
+// });
+cohortRouter.get("/api/cohorts/:cohortId", (req, res) => {
+  const cohortId = req.params.cohortId;
+  Cohort.findById(cohortId)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+cohortRouter.put("/api/cohorts/:cohortId", (req, res) => {
+  const cohortId = req.params.cohortId;
+  Cohort.findByIdAndUpdate(
+    cohortId,
+    {
+      inProgress: req.body.inProgress,
+      cohortSlug: req.body.cohortSlug,
+      cohortName: req.body.cohortName,
+      program: req.body.program,
+      campus: req.body.campus,
+      endDate: req.body.endDate,
+      program: req.body.program,
+      programManager: req.body.programManager || "",
+      leadTeacher: req.body.leadTeacher,
+      totalHours: req.body.totalHours,
+    },
+    { new: true }
+  )
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+cohortRouter.post("/api/cohorts", (req, res) => {
+  Cohort.create({
+    inProgress: req.body.inProgress,
+    cohortSlug: req.body.cohortSlug,
+    cohortName: req.body.cohortName,
+    program: req.body.program,
+    campus: req.body.campus,
+    endDate: req.body.endDate,
+    program: req.body.program,
+    programManager: req.body.programManager || "",
+    leadTeacher: req.body.leadTeacher,
+    totalHours: req.body.totalHours,
+  })
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 });
 
-cohortRouter.get("/api/cohorts", (req, res) => {
-  res.sendFile(__dirname + "");
+cohortRouter.delete("/api/cohorts/:cohortId", (req, res) => {
+  const cohortId = req.params.cohortId;
+  Cohort.findByIdAndDelete(cohortId)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 });
 
 module.exports = cohortRouter;
